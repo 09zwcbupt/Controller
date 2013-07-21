cdedb47b233ff87b5748ad2010cb4c7fecb07d81
richard <09zwcbupt@gmail.com>
2013-07-21 09:38:23 +0800

    multithread echoserver finished

--- multithread_echoserver.py
+++ multithread_echoserver.py
@@ -1,6 +1,6 @@
 import Queue
 import socket
-import libopenflow
+import libopenflow as of
 from functools import partial
 from tornado.ioloop import IOLoop
 import threading
@@ -25,23 +25,37 @@ class switch(threading.Thread):
         print "init for connection", sock.fileno()
         self.sock = sock # for sending pkts
         self.event = event # for waiting pkt_in
+        self._quit = False
         super(switch, self).__init__()
         
     def run(self):
-        while True:
+        while not self._quit:
             print "waiting", sock.getsockname()
             #sock.send("123")
             self.event.wait() #waiting for a packet
+            if self._quit:
+                print "receive quit signal"
+                self.event.clear()
+                return
             #need something to ?
-            print sock.getsockname()
-            #return
+            print self.sock, self.sock.getsockname(), self.sock.getpeername()
+            #try:
             data = self.sock.recv(1024)
+            #except 
             #data = ""
             if data == "":
+                print "close connection"
                 sock.close()
                 return
             self.sock.send(data)
             self.event.clear()
+        print "quit", self._quit
+
+    def stop(self):
+        print "closing thread & connection" 
+        self.sock.close()
+        self.event.set()
+        self._quit = True
     
 def handle_pkt(cli_addr, fd, event):
     s = fd_map[fd]
@@ -73,11 +87,16 @@ def handle_server(fd, event):
         ioloop.add_handler(conn_fd, handle, IOLoop.READ)
         event_map[conn] = threading.Event()
         thread_map[conn_fd] = switch(conn, event_map[conn])
+        print thread_map
         thread_map[conn_fd].start()
 
 ioloop.add_handler(fd, handle_server, IOLoop.READ)
 try:
     ioloop.start()
 except KeyboardInterrupt:
+    print "quit"
+    for t in thread_map:
+        thread_map[t].stop()
+        thread_map[t].join
     ioloop.stop()
-    #print "quit"
+    sock.close()
